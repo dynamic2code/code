1cceaf5764f13f657220075311321bb390a45e75
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-11-29 07:29:29 +0300

    continuation

--- python algo & data structure/lists.py
+++ python algo & data structure/lists.py
@@ -2,16 +2,24 @@
 
 #creating a list 
 
-class List:
+class MyList:
     def __init__(self):
         self.names = []
 
     def addToList(self):
         #the lst will take inputs from user 
-        userInPut = [input("enter your inputs: ").split()]
+        userInPut = input("enter your inputs: ").split()
         self.names.append(userInPut)
         print("The new list is: \n {}".format(self.names))
     
     def removeFromList(self):
-        removeWithIndex = int(input("enter the position you want removed: "))
-        removeWithItemName = input("enter the name of the item you wish removed: ")
+        removeWithIndex = int(input("Enter the position you want removed: "))
+        removeWithItemName = input("Enter the name of the item you wish removed: ")
+
+        self.names.pop(removeWithIndex)
+        self.names.remove(removeWithItemName)
+
+if __name__ == "__main__":
+    myList = MyList()
+    myList.addToList()
+    myList.removeFromList()
